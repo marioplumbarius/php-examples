aefe72bd241f3a389729bd5b23125cf0c53a50eb
Mario [Plumbarius] Souza <mariodes@canva.com>
2025-03-09 16:50:43 -0300

    add hello world command

---
+++ cli/src/Command/HelloWorldCommand.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace PhpExamples\Cli\Command;
+
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+#[AsCommand(name: 'cli:hello-world', description: 'You know what.')]
+class HelloWorldCommand extends Command
+{
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $output->writeln('Hello World');
+        return Command::SUCCESS;
+    }
+
+    protected function configure(): void
+    {
+        $this
+            ->setHelp('This command prints "Hello World" to the console.')
+        ;
+    }
+}
